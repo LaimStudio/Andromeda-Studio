3297d6157abb1ab7d01c9a8a1d925f8c9f5462a0
Desktop/Laim0n <laim0n@yandex.ru>
2018-05-12 18:49:18 +0300

    Add tools panel api

---
+++ Andromeda-Studio/Data/Classes/Api.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+
+namespace AndromedaStudio.Data.Classes
+{
+    class Tools
+    {
+        private static bool _autoHidden = true;
+        private static bool _visible = false;
+
+        public static bool AutoHidden
+        {
+            get => _autoHidden;
+            set
+            {
+                _autoHidden = value;
+                if(value == false && _visible == false)
+                {
+                    VisibleAnimation(true);
+                }
+            }
+        }
+
+        public static bool Visible
+        {
+            get => _visible;
+            set
+            {
+                _visible = value;
+                if(AutoHidden == true)
+                {
+                    VisibleAnimation(value);
+                }
+            }
+        }
+
+        private static void VisibleAnimation(bool type)
+        {
+            Database.MainWindow.ToolsCircles.Children.Clear();
+            if(type)
+            {
+                Database.MainWindow.ToolsContent.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Database.MainWindow.ToolsContent.Visibility = Visibility.Hidden;
+                int count = Database.MainWindow.ToolsContent.Children.Count;
+                while (count != 0)
+                {
+                    count--;
+                    Database.MainWindow.ToolsCircles.Children.Add(new Controls.RadioButton());
+                }
+            }
+        }
+
+    }
+}
