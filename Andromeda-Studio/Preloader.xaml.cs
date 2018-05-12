d42455ab3e33efe34a8782e8e12f9c315293eb1f
Desktop/Laim0n <laim0n@yandex.ru>
2018-05-12 18:50:07 +0300

    Add preloader

---
+++ Andromeda-Studio/Preloader.xaml.cs
@@ -0,0 +1,16 @@
+﻿using AndromedaStudio.Data.Classes;
+using System.Windows;
+
+namespace AndromedaStudio
+{
+    public partial class Preloader : Window
+    {
+        public Preloader()
+        {
+            InitializeComponent();
+
+            Database.MainWindow.Show();
+            Hide();
+        }
+    }
+}
