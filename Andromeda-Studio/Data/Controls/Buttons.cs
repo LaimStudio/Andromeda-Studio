9b823cdb77013e62d1bb5da9dde264db35657328
Desktop/Laim0n <laim0n@yandex.ru>
2018-05-12 15:28:02 +0300

    New conponents and update colors

--- Andromeda-Studio/Data/Controls/Buttons.cs
+++ Andromeda-Studio/Data/Controls/Buttons.cs
@@ -59,4 +59,110 @@ namespace AndromedaStudio.Data.Controls
     class TextButton : Button
     {
     }
+
+    class ToggleButton : System.Windows.Controls.Primitives.ToggleButton
+    {
+        #region Properties
+
+        /// <summary>
+        /// EN: Gets or sets the geometry value.
+        /// RU: Получает или задает значение геометрии.
+        /// </summary>
+        public Geometry Icon
+        {
+            get => (Geometry)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        /// <summary>
+        /// EN: Gets or sets the System.Windows.Media.Brush
+        /// RU: Получает или задает System.Windows.Media.Brush
+        /// </summary>
+        public Brush IconFill
+        {
+            get => (Brush)GetValue(IconFillProperty);
+            set => SetValue(IconFillProperty, value);
+        }
+
+        /// <summary>
+        /// EN: Gets or sets the value of the external content field.
+        /// RU: Получает или задает значение внешнего поля содержимого.
+        /// </summary>
+        public double IconSize
+        {
+            get => (double)GetValue(IconSizeProperty);
+            set => SetValue(IconSizeProperty, value);
+        }
+
+        #region DependencyProperties
+
+        public readonly static DependencyProperty IconProperty =
+             DependencyProperty.Register("Icon", typeof(Geometry),
+             typeof(ToggleButton));
+
+        public readonly static DependencyProperty IconFillProperty =
+            DependencyProperty.Register("IconFill", typeof(Brush),
+            typeof(ToggleButton), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(0, 0, 0))));
+
+        public readonly static DependencyProperty IconSizeProperty =
+            DependencyProperty.Register("IconSize", typeof(double),
+            typeof(ToggleButton), new UIPropertyMetadata(16.0));
+
+        #endregion
+
+        #endregion
+    }
+
+    class RadioButton : System.Windows.Controls.RadioButton
+    {
+        #region Properties
+
+        /// <summary>
+        /// EN: Gets or sets the geometry value.
+        /// RU: Получает или задает значение геометрии.
+        /// </summary>
+        public Geometry Icon
+        {
+            get => (Geometry)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        /// <summary>
+        /// EN: Gets or sets the System.Windows.Media.Brush
+        /// RU: Получает или задает System.Windows.Media.Brush
+        /// </summary>
+        public Brush IconFill
+        {
+            get => (Brush)GetValue(IconFillProperty);
+            set => SetValue(IconFillProperty, value);
+        }
+
+        /// <summary>
+        /// EN: Gets or sets the value of the external content field.
+        /// RU: Получает или задает значение внешнего поля содержимого.
+        /// </summary>
+        public double IconSize
+        {
+            get => (double)GetValue(IconSizeProperty);
+            set => SetValue(IconSizeProperty, value);
+        }
+
+        #region DependencyProperties
+
+        public readonly static DependencyProperty IconProperty =
+             DependencyProperty.Register("Icon", typeof(Geometry),
+             typeof(RadioButton));
+
+        public readonly static DependencyProperty IconFillProperty =
+            DependencyProperty.Register("IconFill", typeof(Brush),
+            typeof(RadioButton), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(0, 0, 0))));
+
+        public readonly static DependencyProperty IconSizeProperty =
+            DependencyProperty.Register("IconSize", typeof(double),
+            typeof(RadioButton), new UIPropertyMetadata(16.0));
+
+        #endregion
+
+        #endregion
+    }
 }
