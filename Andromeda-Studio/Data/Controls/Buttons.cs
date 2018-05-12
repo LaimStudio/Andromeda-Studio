4dff0b8de50aaf5369fe1fa3954b4fdb7c7de42f
Desktop/Laim0n <laim0n@yandex.ru>
2018-05-12 11:43:29 +0300

    New buttons

---
+++ Andromeda-Studio/Data/Controls/Buttons.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace AndromedaStudio.Data.Controls
+{
+    class Button : System.Windows.Controls.Button
+    {
+        #region Properties
+
+        /// <summary>
+        /// EN: Gets or sets the geometry value.
+        /// RU: Получает или задает значение геометрии.
+        /// </summary>
+        public Geometry Icon
+        {
+            get => (Geometry)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        /// <summary>
+        /// EN: Gets or sets the System.Windows.Media.Brush
+        /// RU: Получает или задает System.Windows.Media.Brush
+        /// </summary>
+        public Brush IconFill
+        {
+            get => (Brush)GetValue(IconFillProperty);
+            set => SetValue(IconFillProperty, value);
+        }
+
+        /// <summary>
+        /// EN: Gets or sets the value of the external content field.
+        /// RU: Получает или задает значение внешнего поля содержимого.
+        /// </summary>
+        public double IconSize
+        {
+            get => (double)GetValue(IconSizeProperty);
+            set => SetValue(IconSizeProperty, value);
+        }
+
+        #region DependencyProperties
+
+        public readonly static DependencyProperty IconProperty =
+             DependencyProperty.Register("Icon", typeof(Geometry),
+             typeof(Button));
+
+        public readonly static DependencyProperty IconFillProperty =
+            DependencyProperty.Register("IconFill", typeof(Brush),
+            typeof(Button), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(0, 0, 0))));
+
+        public readonly static DependencyProperty IconSizeProperty =
+            DependencyProperty.Register("IconSize", typeof(double),
+            typeof(Button), new UIPropertyMetadata(16.0));
+
+        #endregion
+
+        #endregion
+    }
+
+    class TextButton : Button
+    {
+    }
+}
