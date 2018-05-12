07108f46b9a19b507ceb7a3e6c585b65e5e7b1f0
Desktop/Laim0n <laim0n@yandex.ru>
2018-05-12 22:40:46 +0300

    Add animation

--- Andromeda-Studio/Data/Classes/Api.cs
+++ Andromeda-Studio/Data/Classes/Api.cs
@@ -1,9 +1,14 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Threading.Tasks;
 
 namespace AndromedaStudio.Data.Classes
 {
     class Tools
     {
+        private static bool _lock = false;
         private static bool _autoHidden = true;
         private static bool _visible = false;
 
@@ -16,6 +21,10 @@ namespace AndromedaStudio.Data.Classes
                 if(value == false && _visible == false)
                 {
                     VisibleAnimation(true);
+                }
+                if (value == true && _visible == false)
+                {
+                    VisibleAnimation(false);
                 }
             }
         }
@@ -33,24 +42,61 @@ namespace AndromedaStudio.Data.Classes
             }
         }
 
-        private static void VisibleAnimation(bool type)
+        private async static void VisibleAnimation(bool type)
         {
-            Database.MainWindow.ToolsCircles.Children.Clear();
+            if (_lock)
+                return;
+            _lock = true;
+            var window = Database.MainWindow;
             if(type)
             {
-                Database.MainWindow.ToolsContent.Visibility = Visibility.Visible;
+                Animate.Opacity(window.ToolsContent, 1);
+                Animate.Opacity(window.ToolsCircles, 0);
+                await Task.Delay(300);
+                window.ToolsCircles.Children.Clear();
+                await Task.Delay(100);
+
+                _lock = false;
+                if(_visible == false)
+                {
+                    VisibleAnimation(false);
+                }
             }
             else
             {
-                Database.MainWindow.ToolsContent.Visibility = Visibility.Hidden;
+                Animate.Opacity(window.ToolsContent, 0);
+                Animate.Opacity(window.ToolsCircles, 1);
+
                 int count = Database.MainWindow.ToolsContent.Children.Count;
                 while (count != 0)
                 {
                     count--;
                     Database.MainWindow.ToolsCircles.Children.Add(new Controls.RadioButton());
                 }
+
+                await Task.Delay(500);
+                _lock = false;
+
+                if (_visible == true)
+                {
+                    VisibleAnimation(true);
+                }
             }
         }
 
+    }
+
+    class Animate
+    {
+        public static void Opacity(FrameworkElement sender, double value)
+        {
+            sender.BeginAnimation(Control.OpacityProperty, new DoubleAnimation
+            {
+                To = value,
+                AccelerationRatio = 0.2,
+                DecelerationRatio = 0.7,
+                Duration = TimeSpan.FromMilliseconds(300)
+            });
+        }
     }
 }
