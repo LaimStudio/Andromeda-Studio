f4cf3004e8bc50b49aba5514e1d4d4c3180d690b
Desktop/Laim0n <laim0n@yandex.ru>
2018-05-12 11:44:03 +0300

    Window buttons

--- Andromeda-Studio/MainWindow.xaml.cs
+++ Andromeda-Studio/MainWindow.xaml.cs
@@ -11,6 +11,31 @@ namespace AndromedaStudio
 
         #region GUI
 
+        private void WindowButtons(object sender, RoutedEventArgs e)
+        {
+            var obj = (FrameworkElement)sender;
+            switch(obj.Tag)
+            {
+                case "Close":
+                    Application.Current.Shutdown();
+                    break;
+
+                case "Maximize":
+                    WindowState = WindowState.Maximized;
+                    obj.Tag = "Restore";
+                    break;
+
+                case "Minimize":
+                    WindowState = WindowState.Minimized;
+                    break;
+
+                case "Restore":
+                    WindowState = WindowState.Normal;
+                    obj.Tag = "Maximize";
+                    break;
+            }
+        }
+
         #endregion
     }
 }
