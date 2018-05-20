ca5222e8071e5012856cce20ee9ed853665d13a0
Desktop/Laim0n <laim0n@yandex.ru>
2018-05-20 16:15:41 +0300

    Update

--- Andromeda-Studio/Data/Classes/Api.cs
+++ Andromeda-Studio/Data/Classes/Api.cs
@@ -6,10 +6,9 @@ using System.Threading.Tasks;
 
 namespace AndromedaStudio.Data.Classes
 {
+#pragma warning disable CS4014
     class Tools
     {
-        #pragma warning disable CS4014
-
         private static bool _lock;
         private static bool _visible;
         private static RadioButton _toolChecked;
@@ -166,64 +165,8 @@ namespace AndromedaStudio.Data.Classes
 
             _lockHide = false;
         }
-
     }
-    class Snackbar
-    {
-        private static bool _lock = false;
-
-        public static async void Show(Panel parent, string text)
-        {
-            if(_lock)
-                await WaitLock();
-
-            foreach (FrameworkElement child in parent.Children)
-            {
-                if (child is Controls.Snackbar)
-                {
-                    Animate.Margin(child, new Thickness(25, 0, 0, child.Margin.Bottom + 50));
-                }
-            }
-
-            _lock = true;
-
-            var obj = new Controls.Snackbar
-            {
-                Text = text,
-                HorizontalAlignment = HorizontalAlignment.Left,
-                VerticalAlignment = VerticalAlignment.Bottom,
-            };
-            obj.Margin = new Thickness(25, 0, 0, -45);
-            parent.Children.Add(obj);
-
-            await Animate.Margin(obj, new Thickness(25, 0, 0, 25));
-
-            _lock = false;
-            await Task.Delay(3000);
-
-            _lock = true;
-            if(obj.Margin.Bottom > 25)
-            {
-                Animate.Opacity(obj, 0);
-                await Animate.Margin(obj, new Thickness(25, 0, 0, obj.Margin.Bottom + 25));
-            }
-            else
-            {
-                await Animate.Margin(obj, new Thickness(25, 0, 0, -45));
-            }
-            parent.Children.Remove(obj);
-            _lock = false;
-        }
-
-        async static Task WaitLock()
-        {
-            while (_lock)
-            {
-                await Task.Delay(1);
-            }
-        }
-    }
-
+    
     class Animate
     {
         public static async Task Opacity(FrameworkElement sender, double value, int time = 200)
--- Andromeda-Studio/Data/Controls/Snackbar.xaml.cs
+++ Andromeda-Studio/Data/Controls/Snackbar.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System.Windows;
+﻿using AndromedaStudio.Data.Classes;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace AndromedaStudio.Data.Controls
 {
+#pragma warning disable CS4014
     public partial class Snackbar : UserControl
     {
         public Snackbar() => InitializeComponent();
@@ -14,8 +18,50 @@ namespace AndromedaStudio.Data.Controls
             get => (string)GetValue(TextProperty);
             set => SetValue(TextProperty, value);
         }
-        
+
+        /// <summary>
+        /// EN: Gets or sets the geometry value.
+        /// RU: Получает или задает значение геометрии.
+        /// </summary>
+        public Geometry Icon
+        {
+            get => (Geometry)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        /// <summary>
+        /// EN: Gets or sets the System.Windows.Media.Brush
+        /// RU: Получает или задает System.Windows.Media.Brush
+        /// </summary>
+        public Brush IconFill
+        {
+            get => (Brush)GetValue(IconFillProperty);
+            set => SetValue(IconFillProperty, value);
+        }
+
+        /// <summary>
+        /// EN: Gets or sets the value of the external content field.
+        /// RU: Получает или задает значение внешнего поля содержимого.
+        /// </summary>
+        public double IconSize
+        {
+            get => (double)GetValue(IconSizeProperty);
+            set => SetValue(IconSizeProperty, value);
+        }
+
         #region DependencyProperties
+
+        public readonly static DependencyProperty IconProperty =
+             DependencyProperty.Register("Icon", typeof(Geometry),
+             typeof(Snackbar));
+
+        public readonly static DependencyProperty IconFillProperty =
+            DependencyProperty.Register("IconFill", typeof(Brush),
+            typeof(Snackbar), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(0, 0, 0))));
+
+        public readonly static DependencyProperty IconSizeProperty =
+            DependencyProperty.Register("IconSize", typeof(double),
+            typeof(Snackbar), new UIPropertyMetadata(14.0));
 
         public readonly static DependencyProperty TextProperty =
             DependencyProperty.Register("Text", typeof(string),
@@ -24,5 +70,60 @@ namespace AndromedaStudio.Data.Controls
         #endregion
 
         #endregion
+
+        private bool _lock = false;
+
+        public async void Show(Panel parent)
+        {
+            if (_lock)
+                await WaitLock();
+
+            foreach (FrameworkElement child in parent.Children)
+            {
+                if (child is Snackbar)
+                {
+                    Animate.Margin(child, new Thickness(25, 0, 0, child.Margin.Bottom + 50));
+                }
+            }
+
+            _lock = true;
+            
+            if(Icon != null)
+            {
+                IconContent.Visibility = Visibility.Visible;
+            }
+
+            HorizontalAlignment = HorizontalAlignment.Left;
+            VerticalAlignment = VerticalAlignment.Bottom;
+
+            Margin = new Thickness(25, 0, 0, -45);
+            parent.Children.Add(this);
+
+            await Animate.Margin(this, new Thickness(25, 0, 0, 25));
+
+            _lock = false;
+            await Task.Delay(3000);
+
+            _lock = true;
+            if (Margin.Bottom > 25)
+            {
+                Animate.Opacity(this, 0);
+                await Animate.Margin(this, new Thickness(25, 0, 0, Margin.Bottom + 25));
+            }
+            else
+            {
+                await Animate.Margin(this, new Thickness(25, 0, 0, -45));
+            }
+            parent.Children.Remove(this);
+            _lock = false;
+        }
+
+        async Task WaitLock()
+        {
+            while (_lock)
+            {
+                await Task.Delay(1);
+            }
+        }
     }
 }
