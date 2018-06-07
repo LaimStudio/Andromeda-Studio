69c9ee84f5769c562acbc0e2b3706fe07cc665b9
Laim0n <laim0n@yandex.ru>
2018-06-07 18:50:07 +0300

    Fix

--- Andromeda-Studio/Data/Classes/Database.cs
+++ Andromeda-Studio/Data/Classes/Database.cs
@@ -5,7 +5,9 @@ namespace AndromedaStudio.Data.Classes
         public static MainWindow MainWindow = new MainWindow();
         public static Controls.Menu Menu = new Controls.Menu
         {
-            Visibility = System.Windows.Visibility.Collapsed
+            Visibility = System.Windows.Visibility.Collapsed,
+            HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+            VerticalAlignment = System.Windows.VerticalAlignment.Center
         };
         public static Controls.Tools Tools = new Controls.Tools {
             Visibility = System.Windows.Visibility.Collapsed,
