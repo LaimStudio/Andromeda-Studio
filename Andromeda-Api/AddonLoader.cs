e04e2d86165d62f0d377f035306a47dfe74bfb3f
Exeteres <exeteres@yandex.ru>
2018-06-13 09:13:18 +0700

    Full transition to parallel loading of add-ons

--- Andromeda-Api/AddonLoader.cs
+++ Andromeda-Api/AddonLoader.cs
@@ -32,6 +32,13 @@ namespace AndromedaApi
             }
         }
 
+        public delegate void ExceptionHandler(Exception exception);
+
+        /// <summary>
+        /// Возникает при появлении исключения во время загрузки дополнений
+        /// </summary>
+        public event ExceptionHandler OnException;
+
         /// <summary>
         /// Загружает дополнение из указаного файла
         /// </summary>
@@ -50,11 +57,11 @@ namespace AndromedaApi
         /// <returns></returns>
         public async Task LoadFromDirectoryAsync(string path)
         {
-            await Task.Run(async () =>
+            await Task.Run(() =>
             {
                 foreach (var item in Directory.EnumerateFiles(path, "*.dll"))
                 {
-                    await LoadFromFileAsync(item);
+                    LoadFromFileAsync(item);
                 }
             });
         }
@@ -63,44 +70,21 @@ namespace AndromedaApi
         /// Асинхронно загружает дополнение из указаного файла
         /// </summary>
         /// <param name="path">Абсолютный путь до файла дополнения</param>
-        public async Task LoadFromFileAsync(string path)
+        public async void LoadFromFileAsync(string path)
         {
-            await Task.Run(() =>
+            try
             {
-                var assembly = Assembly.LoadFrom(path);
-                if (assembly.GetCustomAttribute(typeof(AndromedaAddon)) != null)
-                    Addons.Add(new Addon(assembly));
-            });
-        }
-
-        /// <summary>
-        /// Параллельно загружает все дополнения, найденные в указанной папке (экспериментально)
-        /// </summary>
-        /// <param name="path">Абсолютный путь до папки с дополнениями</param>
-        /// <returns></returns>
-        public async Task LoadFromDirectoryParallel(string path)
-        {
-            await Task.Run(() =>
-            {
-                foreach (var item in Directory.EnumerateFiles(path, "*.dll"))
+                await Task.Run(() =>
                 {
-                    LoadFromFileParallel(item);
-                }
-            });
-        }
-
-        /// <summary>
-        /// Асинхронно загружает дополнение из указаного файла и не требует ожидания
-        /// </summary>
-        /// <param name="path">Абсолютный путь до файла дополнения</param>
-        public async void LoadFromFileParallel(string path)
-        {
-            await Task.Run(() =>
+                    var assembly = Assembly.LoadFrom(path);
+                    if (assembly.GetCustomAttribute(typeof(AndromedaAddon)) != null)
+                        Addons.Add(new Addon(assembly));
+                });
+            }
+            catch (Exception e)
             {
-                var assembly = Assembly.LoadFrom(path);
-                if (assembly.GetCustomAttribute(typeof(AndromedaAddon)) != null)
-                    Addons.Add(new Addon(assembly));
-            });
+                OnException?.Invoke(e);
+            }
         }
     }
 }
