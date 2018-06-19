a52d940c4d3907880d6a512eb7d44b4cc303889e
Laim0n <laim0n@yandex.ru>
2018-06-19 23:09:16 +0300

    Add night theme setting and update menu page

--- Andromeda-Studio/Data/Controls/Menu/Pages/Settings/General.xaml.cs
+++ Andromeda-Studio/Data/Controls/Menu/Pages/Settings/General.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AndromedaStudio.Data.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,11 +18,62 @@ namespace AndromedaStudio.Data.Controls.MenuPages.Setting
 {
     public partial class General : Page
     {
-        public General() => InitializeComponent();
+        public General()
+        {
+            InitializeComponent();
+
+            foreach(FrameworkElement obj in Body.Children)
+            {
+                var par = (string)obj.Tag;
+                if (par == "NightTheme")
+                {
+                    var objec = (CheckBox)obj;
+                    if (App.Theme == "Night")
+                    {
+                        objec.IsChecked = true;
+                    }
+                }
+            }
+        }
 
         private void Menu_Select(object sender, RoutedEventArgs e)
         {
-            Data.Classes.Menu.SetPage(sender);
+            Classes.Menu.SetPage(sender);
+        }
+
+        async private void Checkbox(object sender, RoutedEventArgs e)
+        {
+            var obj = (CheckBox)sender;
+            var par = (string)obj.Tag;
+
+
+            if (obj.IsChecked == true)
+            {
+                if(par == "NightTheme")
+                {
+                    RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap((int)Database.MainWindow.Body.ActualWidth, (int)Database.MainWindow.Body.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+                    renderTargetBitmap.Render(Database.MainWindow.Body);
+                    var img = new Image { Source = renderTargetBitmap };
+                    Database.MainWindow.Body.Children.Add(img);
+                    App.Theme = "Night";
+                    await Animate.Opacity(img, 0);
+                    Database.MainWindow.Body.Children.Remove(img);
+                }
+            }
+
+            if (obj.IsChecked == false)
+            {
+                if (par == "NightTheme")
+                {
+                    RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap((int)Database.MainWindow.Body.ActualWidth, (int)Database.MainWindow.Body.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+                    renderTargetBitmap.Render(Database.MainWindow.Body);
+                    var img = new Image { Source = renderTargetBitmap };
+                    Database.MainWindow.Body.Children.Add(img);
+                    App.Theme = "Day";
+                    await Animate.Opacity(img, 0);
+                    Database.MainWindow.Body.Children.Remove(img);
+                }
+            }
         }
     }
 }
