bf6cd02a1335e1e7fc9ecf4ec6f0b6a94c2327a9
Laim0n <laim0n@yandex.ru>
2018-06-29 03:27:42 +0300

    Refactor

--- Andromeda-Studio/Data/Classes/Database.cs
+++ Andromeda-Studio/Data/Classes/Database.cs
@@ -9,7 +9,8 @@ namespace AndromedaStudio.Data.Classes
             HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
             VerticalAlignment = System.Windows.VerticalAlignment.Center
         };
-        public static Controls.Tools Tools = new Controls.Tools {
+        public static Controls.Tools Tools = new Controls.Tools
+        {
             Visibility = System.Windows.Visibility.Collapsed,
             HorizontalAlignment = System.Windows.HorizontalAlignment.Right,
             VerticalAlignment = System.Windows.VerticalAlignment.Bottom
