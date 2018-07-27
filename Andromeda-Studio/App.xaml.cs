c92597ea102f9e3c8e6b62359ecb61216d937790
Laim0n <laim0n@yandex.ru>
2018-07-28 00:39:43 +0300

    Add language change mehanism and refactor

--- Andromeda-Studio/App.xaml.cs
+++ Andromeda-Studio/App.xaml.cs
@@ -16,10 +16,78 @@ namespace AndromedaStudio
     /// </summary>
     public partial class App : Application
     {
+
+        #region Languages
+        private static List<CultureInfo> m_Languages = new List<CultureInfo>();
+
+        public static List<CultureInfo> Languages
+        {
+            get
+            {
+                return m_Languages;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
+            App.LanguageChanged += App_LanguageChanged;
+
+            m_Languages.Clear();
+            m_Languages.Add(new CultureInfo("en-US"));
+            m_Languages.Add(new CultureInfo("ru-RU"));
+            m_Languages.Add(new CultureInfo("zh-ZH"));
         }
+        
+        public static event EventHandler LanguageChanged;
+
+        public static CultureInfo Language
+        {
+            get
+            {
+                return System.Threading.Thread.CurrentThread.CurrentUICulture;
+            }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("value");
+                if (value == System.Threading.Thread.CurrentThread.CurrentUICulture) return;
+
+                System.Threading.Thread.CurrentThread.CurrentUICulture = value;
+                
+                ResourceDictionary dict = new ResourceDictionary();
+                dict.Source = new Uri(String.Format("Data/Languages/{0}.xaml", value.Name), UriKind.Relative);
+                
+                ResourceDictionary oldDict = null;
+                try
+                {
+                    oldDict = (from d in Current.Resources.MergedDictionaries
+                               where d.Source != null && d.Source.OriginalString.StartsWith("Data/Languages/")
+                               select d).First();
+                }
+                catch (Exception) { }
+                
+                if (oldDict != null)
+                {
+                    int ind = Current.Resources.MergedDictionaries.IndexOf(oldDict);
+                    Current.Resources.MergedDictionaries.Remove(oldDict);
+                    Current.Resources.MergedDictionaries.Insert(ind, dict);
+                }
+                else
+                {
+                    Current.Resources.MergedDictionaries.Add(dict);
+                }
+                
+                LanguageChanged(Current, new EventArgs());
+            }
+        }
+
+        private void App_LanguageChanged(Object sender, EventArgs e)
+        {
+            //save
+        }
+        #endregion
+
+        #region Theme
         private static string _theme;
 
         public static string Theme
@@ -56,5 +124,6 @@ namespace AndromedaStudio
                 }
             }
         }
+        #endregion
     }
 }
