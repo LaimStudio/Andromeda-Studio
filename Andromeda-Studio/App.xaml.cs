cf9fbb2cf636fbb1ddfbbf3db9a4d14b948de842
Laim0n <laim0n@yandex.ru>
2018-08-22 23:33:11 +0300

    Refactor

--- Andromeda-Studio/App.xaml.cs
+++ Andromeda-Studio/App.xaml.cs
@@ -1,12 +1,8 @@
-﻿using AndromedaStudio.Classes;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Data;
 using System.Globalization;
 using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace AndromedaStudio
--- Andromeda-Studio/Data/Controls/Buttons/ProfileToggleButton.cs
+++ Andromeda-Studio/Data/Controls/Buttons/ProfileToggleButton.cs
@@ -1,6 +1,3 @@
-﻿using System.Windows;
-using System.Windows.Media;
-
 namespace AndromedaStudio.Controls
 {
     class ProfileToggleButton : ToggleButton
