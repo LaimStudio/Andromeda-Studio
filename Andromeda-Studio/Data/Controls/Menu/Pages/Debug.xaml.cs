94f3b4759e68d6b685e2bf7b41bdd9074dcd8fa6
Laim0n <laim0n@yandex.ru>
2018-08-24 12:40:20 +0300

    Menu page update and add python highlighting

--- Andromeda-Studio/Data/Controls/Menu/Pages/Debug.xaml.cs
+++ Andromeda-Studio/Data/Controls/Menu/Pages/Debug.xaml.cs
@@ -1,19 +1,27 @@
 ﻿using System.Windows.Controls;
-using System.Diagnostics;
-using System.Windows;
 using ICSharpCode.AvalonEdit.Highlighting;
+using System.Xml;
+using System.IO;
+using System.Reflection;
 
 namespace AndromedaStudio.Controls.MenuPages
 {
     public partial class Debug : Page
     {
-        private IHighlightingDefinition _highlighting;
         public Debug()
         {
+            IHighlightingDefinition customHighlighting;
+            using (Stream s = Assembly.GetExecutingAssembly().GetManifestResourceStream("AndromedaStudio.Data.Themes.Highlightings.Day.Python.xshd"))
+            {
+                using (XmlReader reader = new XmlTextReader(s))
+                {
+                    customHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.
+                        HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
 
+            HighlightingManager.Instance.RegisterHighlighting("Python", new string[] { ".cool" }, customHighlighting);
             InitializeComponent();
-            _highlighting = HighlightingManager.Instance.GetDefinitionByExtension(".py");
-            DataContext = this;
         }
     }
 }
