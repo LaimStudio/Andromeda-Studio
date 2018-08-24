5f0893a4c317d1966ee21395015231cc28953007
Laim0n <laim0n@yandex.ru>
2018-08-24 18:07:46 +0300

    Update notifications

--- Andromeda-Studio/Data/Classes/Notifications/Notification.cs
+++ Andromeda-Studio/Data/Classes/Notifications/Notification.cs
@@ -12,11 +12,21 @@ namespace AndromedaStudio.Notifications
             set => SetValue(DescriptionProperty, value);
         }
 
+        public double ProgressValue
+        {
+            get => (double)GetValue(ProgressValueProperty);
+            set => SetValue(ProgressValueProperty, value);
+        }
+
         #region DependencyProperties
 
         public readonly static DependencyProperty DescriptionProperty =
              DependencyProperty.Register("Description", typeof(string),
              typeof(Notification));
+
+        public readonly static DependencyProperty ProgressValueProperty =
+             DependencyProperty.Register("ProgressValue", typeof(double),
+             typeof(Notification), new PropertyMetadata(0.0));
 
         #endregion
 
